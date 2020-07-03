2e805f62cdaf2e9e7e6a0a371352820db76b6246
Marnix Klooster <marnix.klooster@gmail.com>
2020-07-03 21:16:30 +0200

    calling generating function

--- gen.zig
+++ gen.zig
@@ -1,21 +1,34 @@
 const std = @import("std");
+const assert = std.debug.assert;
+
+// TODO list:
+//  - implement yield();
+//  - non-void return type
 
 fn GenOf(comptime T: type) type {
     return struct {
         const Self = @This();
         fn create(f: var) Self {
-            return Self{};
+            var self = Self{};
+            _ = async f.@""(self);
+            return self;
         }
     };
 }
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
+var called = false;
+fn nats(g: var, n: u16) void {
+    called = true;
+}
+
 test "" {
-    const RetType = void;
-    const nats = GenOf(u16).create(struct {
-        fn @""(g: var) RetType {
-            return nats(g, 3);
+    const nrs = GenOf(u16).create(struct {
+        fn @""(g: var) void {
+            return nats(g, 1);
         }
     });
+
+    assert(called);
 }
