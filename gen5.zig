076f863467f029ceed39d975179478ffa4052963
Marnix Klooster <marnix.klooster@gmail.com>
2020-11-04 16:10:14 +0100

    gen5.zig: allocate sink, merge create() and init()

--- gen5.zig
+++ gen5.zig
@@ -2,45 +2,56 @@ const std = @import("std");
 
 const Allocator = std.mem.Allocator;
 
-fn SequenceIterator(comptime S: type, comptime V: type) type {
+fn Sink(comptime V: type) type {
     return struct {
         const Self = @This();
         const State = enum { empty, filled, uninitialized };
 
         _state: union(State) { uninitialized, empty: void, filled: V } = .uninitialized,
-        _frame: @Frame(S.@"") = undefined,
-
-        inline fn create() Self {
-            return Self{};
-        }
-
-        inline fn init(self: *Self, sequence: *S) *Self {
-            std.debug.assert(self._state == .uninitialized);
-            self._frame = async sequence.@""(self); // can go to state .filled, in yielding()
-            return self;
-        }
-
-        fn next(self: *Self) ?V {
-            // run the generator again, if necessary
-            if (self._state == .empty) {
-                self._state = .uninitialized;
-                resume self._frame; // can go to state .filled, in yielding()
-            }
-            std.debug.assert(self._state != .empty);
-            // return the next result of the generator
-            if (self._state == .filled) {
-                const result = self._state.filled;
-                self._state = .empty;
-                return result;
-            } else {
-                std.debug.assert(self._state == .uninitialized);
-                return null; // end of generator
-            }
-        }
 
         fn yielding(self: *Self, v: V) void {
             std.debug.assert(self._state != .filled);
             self._state = .{ .filled = v };
+        }
+    };
+}
+
+fn SequenceIterator(comptime S: type, comptime V: type) type {
+    return struct {
+        const Self = @This();
+
+        _allocator: *Allocator,
+        _sink: *Sink(V),
+        _frame: @Frame(S.@""),
+
+        inline fn create(allocator: *Allocator, sequence: *S) !Self {
+            var sink = try allocator.create(Sink(V));
+            sink.* = Sink(V){};
+            std.debug.assert(sink._state == .uninitialized);
+            const frame = async sequence.@""(sink); // can go to state .filled, in yielding()
+            return Self{ ._allocator = allocator, ._sink = sink, ._frame = frame };
+        }
+
+        fn deinit(self: *Self) void {
+            self._allocator.destroy(self._sink);
+        }
+
+        fn next(self: *Self) ?V {
+            // run the generator again, if necessary
+            if (self._sink._state == .empty) {
+                self._sink._state = .uninitialized;
+                resume self._frame; // can go to state .filled, in yielding()
+            }
+            std.debug.assert(self._sink._state != .empty);
+            // return the next result of the generator
+            if (self._sink._state == .filled) {
+                const result = self._sink._state.filled;
+                self._sink._state = .empty;
+                return result;
+            } else {
+                std.debug.assert(self._sink._state == .uninitialized);
+                return null; // end of generator
+            }
         }
     };
 }
@@ -51,7 +62,7 @@ const expect = std.testing.expect;
 const Range = struct {
     const Self = @This();
     m: usize,
-    fn @""(self: *Self, gen: *SequenceIterator(Self, usize)) void {
+    fn @""(self: *Self, gen: *Sink(usize)) void {
         var i: usize = 0;
         while (i < self.m) : (i += 1) {
             suspend gen.yielding(i);
@@ -67,7 +78,8 @@ test "" {
     var r: ?usize = null;
 
     // TODO: Can the type Range be inferred by SequenceIterator()?
-    var it = SequenceIterator(Range, usize).create().init(&range(2));
+    var it = try SequenceIterator(Range, usize).create(std.testing.allocator, &range(2));
+    defer it.deinit();
 
     r = it.next();
     expect(r.? == 0);
@@ -80,8 +92,13 @@ test "" {
 
     r = it.next();
     expect(r == null);
+}
 
-    it = SequenceIterator(Range, usize).create().init(&range(0));
+test "2" {
+    var r: ?usize = null;
+
+    var it = try SequenceIterator(Range, usize).create(std.testing.allocator, &range(0));
+    defer it.deinit();
 
     r = it.next();
     expect(r == null);
